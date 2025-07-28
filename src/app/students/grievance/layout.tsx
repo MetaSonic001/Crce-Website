c6ff654cc8a467f582414f6c490dd49c207e062e
Chris-Lopes <chrislopes3667@gmail.com>
2025-07-28 19:02:29 +0530

    c

--- src/app/students/grievance/layout.tsx
+++ src/app/students/grievance/layout.tsx
@@ -1,15 +1,7 @@
 import { Inter } from 'next/font/google'
 import '../../globals.css'
-import Navbar from '@/components/navbar'
-import Footer from '@/components/footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <section className={inter.className}>
-      <Navbar />
-      {children}
-      <Footer />
-    </section>
-  )
+  return <section className={inter.className}>{children}</section>
 }
